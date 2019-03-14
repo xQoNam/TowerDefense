056605687290f6d5d8ae711718a9c31f512d37e8
xQoNam <kandeckigs1234@interia.pl>
2019-03-14 21:33:23 +0100

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]private GameObject tile;
+
+    public float TileSize
+    {
+        get { return tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateLevel()
+    {
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        for (int y = 0; y < 10; y++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                PlaceTile(y, x, worldStart);
+            }
+        }
+    }
+
+    private void PlaceTile(int y, int x, Vector3 worldStart)
+    {
+        GameObject newTile = Instantiate(tile);
+        newTile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+    }
+}
